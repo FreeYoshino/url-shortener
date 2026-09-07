e0a940b08694307fdbea80146e90c41c3924ea32
FreeYoshino <abc0940415@gmail.com>
2026-09-07 20:58:11 +0800

    feat(prisma): use PrismaPg adapter for PostgreSQL

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,24 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private pool: Pool;
+  constructor() {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({ adapter });
+
+    this.pool = pool;
+  }
+
   async onModuleInit() {
     // Connect to the database when the module is initialized
     await this.$connect();
